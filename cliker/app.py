5abde8a807b2a3e2c756d7af17bb463d58dfd3a9
BalenciagoRus <vsohareva61@gmail.com>
2025-11-24 19:35:24 +0300

    Add files via upload

--- cliker/app.py
+++ cliker/app.py
@@ -1,25 +1,9 @@
 from flask import Flask, render_template_string, send_from_directory
-
-app = Flask(__name__)
-
-# Загрузка содержимого index.html
-try:
-    with open('index.html', 'r', encoding='utf-8') as f:
-        html_content = f.read()
-except FileNotFoundError:
-    html_content = "<h1>Ошибка: Не найден index.html</h1>"
-
-@app.route('/')
-def index():
-    # Отправляет index.html в браузер
-    return render_template_string(html_content)
-
-@app.route('/game.js')
-def serve_js():
-    # Отправляет game.js, который запрашивает браузер
-    return send_from_directory('.', 'game.js')
+# ... (весь ваш остальной код) ...
 
 if __name__ == '__main__':
-    # Запуск сервера на http://127.0.0.1:5000/
+    # Эта часть будет работать только локально для тестирования
     print("Сервер запущен на: http://127.0.0.1:5000/")
     app.run(debug=True)
+
+# При деплое Gunicorn будет вызывать объект 'app' напрямую
